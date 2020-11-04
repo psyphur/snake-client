8f7652c74e115d75ffaed79c1af34494f833e58f
Shaun Yap <me@shaunyap.ca>
2020-11-04 20:15:05 +0000

    Added keystroke event handlers

--- play.js
+++ play.js
@@ -1,4 +1,22 @@
 const connect = require('./client');
 
+const setupInput = () => {
+  const stdin = process.stdin;
+  stdin.setRawMode(true); // allows for use of ctrl + c
+  stdin.setEncoding('utf8');
+  stdin.resume();
+  stdin.on('data', (key) => {
+    handleUserInput(key);
+  })
+  return stdin;
+}
+
+const handleUserInput = (key) => {
+  if (key === '\u0003') {
+    process.exit();
+  }
+}
+
 console.log("Connecting...");
 connect();
+setupInput();
